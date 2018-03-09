01fbe705bc682f756ca5659c7283563f0768d181
Joseph Livengood <JFLivengood@gmail.com>
2018-03-09 00:56:08 -0500

    String challenge unit tests built

--- CTCI/main.cpp
+++ CTCI/main.cpp
@@ -1,8 +1,17 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
 #include "test/arraysandstrings_test.cpp"
 #include "test/linkedlists_test.cpp"
 
+TEST_CASE( "1: All test cases reside in `/test` .cpp files", "[multi-file:1]" ) {
+  
+}
+
+/*
 int main(int argc, const char * argv[]) {
   run_arraysandstrings_tests();
   run_linkedlists_tests();
   return 0;
 }
+*/
--- CTCI/test/arraysandstrings_test.cpp
+++ CTCI/test/arraysandstrings_test.cpp
@@ -1,27 +1,63 @@
 #include <iostream>
 
+#include "catch.hpp"
+
 #include "../src/arraysandstrings.cpp"
 
-void test_only_unique_chars() {
-  std::cout << "-----1.1 only_unique_chars-----" << std::endl;
-  std::string str = "abcdzxcvwertyuijhgfdsa";
-  std::cout << str << std::endl;
-  std::cout << only_unique_chars(&str) << std::endl;
+TEST_CASE( "Function `only_unique_chars` returns true if a string has only unique characters", "[arraysandstrings][chapterone]" ) {
+  SECTION( "All unique chars present returns true" ) {
+    std::string str { "abcdefghijkl" };
+    REQUIRE( only_unique_chars(&str) );
+  }
+  SECTION( "Duplicate chars present returns false" ) {
+    std::string str { "abcdefabcxef" };
+    REQUIRE_FALSE( only_unique_chars(&str) );
+  }
+  SECTION( "Duplicate mixed-case chars present returns false" ) {
+    std::string str { "ABCabc" };
+    REQUIRE_FALSE( only_unique_chars(&str) );
+  }
+  SECTION( "Empty string throws exception" ) {
+    std::string str;
+    REQUIRE_THROWS_WITH( only_unique_chars(&str), "Empty string is both unique and not unique." );
+  }
 }
 
-void test_reverse() {
-  std::cout << "-----1.2 reverse-----" << std::endl;
-  char str[] = "Hello World";
-  std::cout << str << std::endl;
-  reverse(str);
-  std::cout << str << std::endl;
+TEST_CASE( "Function `reverse` reverses the characters of a C-style string in place", "[arraysandstrings][chapterone]" ) {
+  SECTION( "Normal string reverses as expected" ) {
+    char forwards[] = "Hello World";
+    char reversed[] = "dlroW olleH";
+    reverse(forwards);
+    REQUIRE( strcmp(forwards,reversed) == 0 );
+  }
+  SECTION( "Empty string throws exception" ) {
+    char forwards[0];
+    REQUIRE_THROWS_WITH( reverse(forwards), "Empty string cannot be reversed." );
+  }
 }
 
-void test_bad_compression() {
-  std::cout << "-----1.5 bad_compression-----" << std::endl;
-  std::string str2 = "aaaabbbsbsdddddgerrr";
-  std::cout << str2 << std::endl;
-  std::cout << bad_compression(str2) << std::endl;
+TEST_CASE( "Function `bad_compression` performs a bad compression", "[arraysandstrings][chapterone]" ) {
+  SECTION( "Normal string compresses as expected" ) {
+    std::string uncompressed = "aabbbcc";
+    std::string act_compressed = bad_compression(uncompressed);
+    std::string exp_compressed = "a2b3c2";
+    REQUIRE( act_compressed == exp_compressed );
+  }
+  SECTION( "Compression is never longer" ) {
+    std::string test_cases[] {
+      "asdachbhjbhkhjjsdhbchee",
+      "hhagdndhddkdhehet",
+      "iiihsnwnwwwjfbche",
+      "qwertyqwertyqqwweerrttyy"
+    };
+    for (const std::string& str : test_cases) {
+      REQUIRE( bad_compression(str).length() <= str.length() );
+    }
+  }
+  SECTION( "No invalid characters are present, most importantly numbers" ) {
+    std::string str = "aa4bbb9cc";
+    REQUIRE_THROWS_WITH( bad_compression(str), "Invalid character in string." );
+  }
 }
 
 void test_rotate_img() {
@@ -71,9 +107,6 @@ void test_zero_intercepts() {
 }
 
 void run_arraysandstrings_tests() {
-  test_only_unique_chars();
-  test_reverse();
-  test_bad_compression();
   test_rotate_img();
   test_zero_intercepts();
 }
